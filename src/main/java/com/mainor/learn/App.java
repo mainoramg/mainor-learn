84760aee21c089f75235d5dd32468c16b04ac0c7
Mainor Miranda <mainor.miranda@bvmedia.cr>
2019-11-20 08:42:08 -0600

    HashMap to HTML

--- src/main/java/com/mainor/learn/App.java
+++ src/main/java/com/mainor/learn/App.java
@@ -12,8 +12,17 @@ public class App
 //        System.out.println( "Test for lastFour(456789, 1254): " + PlayingWithString.lastFour("456789", "1254") );
 //        System.out.println( "Test for lastFour(456789, ): " + PlayingWithString.lastFour("456789", "") );
 //        System.out.println( "Test for lastFour(456789, null): " + PlayingWithString.lastFour("456789", null) );
-        PlayingWithEnum test = new PlayingWithEnum();
-        System.out.println( "isVoid true: " + test.letsChangeTheType(true, PlayingWithEnum.Type.VOID).toString() );
-        System.out.println( "isVoid false: " + test.letsChangeTheType(false, PlayingWithEnum.Type.VOID).toString() );
+
+//        PlayingWithEnum test = new PlayingWithEnum();
+//        System.out.println( "isVoid true: " + test.letsChangeTheType(true, PlayingWithEnum.Type.VOID).toString() );
+//        System.out.println( "isVoid false: " + test.letsChangeTheType(false, PlayingWithEnum.Type.VOID).toString() );
+
+        MapToHtml mapToHtml = new MapToHtml();
+        System.out.println(mapToHtml.mapToHtml(mapToHtml.getData()));
+
+        String downloads = "[test]";
+        downloads = downloads.substring(1);
+        downloads = downloads.substring(0, downloads.length() - 1);
+        System.out.println("downloads="+downloads);
     }
 }
---
+++ src/main/java/com/mainor/learn/MapToHtml.java
@@ -0,0 +1,62 @@
+package com.mainor.learn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapToHtml {
+    HashMap data;
+
+    public MapToHtml(HashMap data) {
+        this.data = data;
+    }
+
+    public MapToHtml() {
+        HashMap child = new HashMap();
+        child.put("puesto", "programador");
+        child.put("salario", "1000");
+        child.put("start", "feb 14");
+
+        HashMap data = new HashMap();
+        data.put("firstName", "Mainor");
+        data.put("lastName", "Miranda");
+        data.put("work", child);
+        data.put("email", "mainoramg@gmail.com");
+
+        this.data = data;
+    }
+
+    public HashMap getData() {
+        return data;
+    }
+
+    public void setData(HashMap data) {
+        this.data = data;
+    }
+
+    public String mapToHtml(HashMap data) {
+        StringBuilder html = new StringBuilder();
+        if (!data.isEmpty()) {
+            html.append("<ul>");
+            for (Object set : data.entrySet()) {
+                Map.Entry entry = (Map.Entry) set;
+                if (entry.getValue() instanceof HashMap) {
+                    html.append("<li>")
+                            .append("<b>")
+                                .append(entry.getKey().toString()).append(": ")
+                            .append("</b>")
+                            .append(mapToHtml((HashMap) entry.getValue()))
+                        .append("</li>");
+                } else if (entry.getValue()  instanceof String) {
+                    html.append("<li>")
+                            .append("<b>")
+                                .append(entry.getKey().toString()).append(": ")
+                            .append("</b>")
+                            .append(entry.getValue().toString())
+                        .append("</li>");
+                }
+            }
+            html.append("</ul>");
+        }
+        return html.toString();
+    }
+}
