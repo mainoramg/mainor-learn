07f9ada50f6dddf0aaf5538b8ddd3806cc7c5bc2
mainor <mainor.miranda@bvmedia.cr>
2019-12-31 15:16:56 -0600

    Chapter 3: Program Control Statements

--- src/main/java/com/mainor/learn/App.java
+++ src/main/java/com/mainor/learn/App.java
@@ -36,5 +36,24 @@ public class App
         System.out.println("Month: "+playingWithDates.getMonth());
         System.out.println("Day: "+playingWithDates.getDay());
         System.out.println("Year: "+playingWithDates.getYear());
+
+        int i;
+
+        for (i=0; i<=5; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i is less than one");
+                case 1:
+                    System.out.println("i is less than two");
+                case 2:
+                    System.out.println("i is less than three");
+                    break;
+                case 3:
+                    System.out.println("i is less than four");
+                case 4:
+                    System.out.println("i is less than five");
+            }
+            System.out.println();
+        }
     }
 }
